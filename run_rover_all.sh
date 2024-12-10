eeb59f03320f7172f43a6de6c5308996a8aab1a6
Fabian Schmidt <112087050+FaSch87@users.noreply.github.com>
2024-12-10 15:14:46 +0100

    Update run_rover_all.sh

--- run_rover_all.sh
+++ run_rover_all.sh
@@ -5,8 +5,7 @@ dates=("2023-08-18" "2023-09-15" "2024-01-13" "2024-04-11" "2024-05-29_1" "2024-
 for date in "${dates[@]}"; do
     echo "Processing date: $date"
 
-    python coslam_rover.py --config configs/ROVER/d435i.yaml --input_folder /workspace/mounted_directory/media/fabian/data_recording_r/kwald/drosselweg/flaeche1/$date/tum/d435i --output output/short/$date
-
+    python coslam_rover.py --config configs/ROVER/d435i.yaml --input_folder /workspace/mounted_directory/media/$date/d435i --output output/$date
 done
 
 echo "All dates processed."
